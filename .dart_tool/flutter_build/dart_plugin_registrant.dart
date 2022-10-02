1f00b6c0c26d8f56b2b6cebfe857212cc9613cde
kstar0707 <prince.kzear1@gmail.com>
2022-10-02 22:58:10 +0600

    appbar design with icon and logo

---
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,110 @@
+//
+// Generated file. Do not edit.
+// This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
+//
+
+// @dart = 2.18
+
+import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:path_provider_android/path_provider_android.dart';
+import 'package:local_auth_ios/local_auth_ios.dart';
+import 'package:path_provider_ios/path_provider_ios.dart';
+import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:path_provider_macos/path_provider_macos.dart';
+import 'package:local_auth_windows/local_auth_windows.dart';
+import 'package:path_provider_windows/path_provider_windows.dart';
+
+@pragma('vm:entry-point')
+class _PluginRegistrant {
+
+  @pragma('vm:entry-point')
+  static void register() {
+    if (Platform.isAndroid) {
+      try {
+        LocalAuthAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`local_auth_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        PathProviderAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isIOS) {
+      try {
+        LocalAuthIOS.registerWith();
+      } catch (err) {
+        print(
+          '`local_auth_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        PathProviderIOS.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isLinux) {
+      try {
+        PathProviderLinux.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isMacOS) {
+      try {
+        PathProviderMacOS.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    } else if (Platform.isWindows) {
+      try {
+        LocalAuthWindows.registerWith();
+      } catch (err) {
+        print(
+          '`local_auth_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        PathProviderWindows.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+    }
+  }
+}
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.devices)),
+                Tab(icon: Icon(Icons.history)),
+                Tab(icon: Icon(Icons.qr_code)),
+              ],
+            ),
+            title: Image.asset(
+              'assets/images/signature.png',
+              fit: BoxFit.cover,
+              height: 25,
+            ),
+            centerTitle: true,
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.devices),
+              Icon(Icons.history),
+              Icon(Icons.qr_code),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
