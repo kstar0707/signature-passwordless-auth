827f2d27ea0e3cfd24dea3d878defd71f1b85210
kstar0707 <prince.kzear1@gmail.com>
2022-10-04 17:25:44 +0600

    fix issue

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const SettingsScreen()),
+                        builder: (context) => const SettingsPage()),
                   );
                 },
               ),
