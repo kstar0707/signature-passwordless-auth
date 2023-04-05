b253dc5217f002a84c2453b33e148aad0ceaadc2
kstar0707 <prince.kzear1@gmail.com>
2023-04-05 19:34:02 +0600

    settings page design comlete

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -8,6 +9,9 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  int groupValue = 1;
+  List<String> subTitle = ["French", "English", "Spanish"];
+  String name = "John";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,8 +57,60 @@ class _SettingsState extends State<Settings> {
             },
           ),
         ),
-        body: const Center(
-          child: Text('Settings'),
+        body: SettingsList(
+          sections: [
+            SettingsSection(
+              title: const Text('Account'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.account_tree_outlined),
+                  title: Text('+880 17 * * * * * 222'.toString()),
+                  value: const Text('Phone number'),
+                ),
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.private_connectivity_rounded),
+                  title: Text('6b 86 b2 73 ff 34 fc e1'.toUpperCase()),
+                  value: const Text('Device ID'),
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: const Text('System'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.fingerprint_outlined),
+                  title: const Text('App Lock'),
+                  value: const Text('Disable'),
+                ),
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.backup_outlined),
+                  title: const Text('Backup and Restore'),
+                  value: const Text('Google Drive (Encrypted)'),
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: const Text('Advanced'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.policy_outlined),
+                  title: const Text('Terms of Service'),
+                ),
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.privacy_tip_outlined),
+                  title: const Text('Privacy Policy'),
+                ),
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.feedback_outlined),
+                  title: const Text('Feedback'),
+                ),
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.info_outline),
+                  title: const Text('About'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
