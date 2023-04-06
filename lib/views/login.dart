2d5c96b77e7bee2e83e345b5e019d3a05dbdfa78
kstar0707 <prince.kzear1@gmail.com>
2023-04-07 05:31:48 +0600

    change somethhing

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:signature/views/applock.dart';
 import 'package:signature/views/homepage.dart';
 
 class Login extends StatefulWidget {
@@ -32,7 +33,7 @@ class _LoginState extends State<Login> {
     if (user != null) {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const Homepage()),
+        MaterialPageRoute(builder: (context) => const LockScreen()),
       );
     }
     //  else {
